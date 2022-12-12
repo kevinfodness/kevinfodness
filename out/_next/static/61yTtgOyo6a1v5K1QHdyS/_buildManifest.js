62582a321c8a4b1ba572c90889b15875a664b065
Kevin Fodness <2650828+kevinfodness@users.noreply.github.com>
2022-12-11 20:53:06 -0500

    Re-upgrade to node 18, add outdir to avoid building on server

---
+++ out/_next/static/61yTtgOyo6a1v5K1QHdyS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/05131c07932f10cd.css","static/chunks/pages/index-97b75b0fb9606e88.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
